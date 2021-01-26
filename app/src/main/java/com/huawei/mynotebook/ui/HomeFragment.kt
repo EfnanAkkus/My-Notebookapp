81ce1f96c761308c74393d7fafd2e50d27bbd202
e84166412 <efnan.akkus@huawei.com>
2021-01-26 17:11:39 +0300

    finished all design and added kotlin plugin

--- app/src/main/java/com/huawei/mynotebook/ui/HomeFragment.kt
+++ app/src/main/java/com/huawei/mynotebook/ui/HomeFragment.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.Navigation.findNavController
 import com.huawei.mynotebook.R
-
+import kotlinx.android.synthetic.main.fragment_home.*
+import java.util.*
 
 
 class HomeFragment : Fragment() {
 
-
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        floatingButtonAdd.setOnClickListener {
+            val action = HomeFragmentDirections.actionAddNote()
+            Navigation.findNavController(it).navigate(action)
+        }
+
     }
 
 }
